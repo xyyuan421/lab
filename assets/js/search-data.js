93e33ec11f67d4cc22844b167ac936c635c37e5e
xyyuan421 <147831339+xyyuan421@users.noreply.github.com>
2023-10-13 23:04:29 +0200

    Add files via upload

---
+++ assets/js/search-data.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "Welcome to Attention and Numerosity lab!",
+        "excerpt":"We’re interested in the nature of attention and numerosity. Jekyll requires blog post files to be named according to the following format: YEAR-MONTH-DAY-title.MARKUP Where YEAR is a four-digit number, MONTH and DAY are both two-digit numbers, and MARKUP is the file extension representing the format used in the file. After...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "http://localhost:4000/jekyll/update/2023/10/13/welcome-to-jekyll.html"
+      }]
